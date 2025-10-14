02ad6efeb1568f307b309413b6fe897d22701e75
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-10-14 12:55:57 +0700

    feat: nambahin asset ikon + tampilan dasar dashboard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     // Optional - Retrofit (untuk login API)
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
