6e8d7fc908b8f03f7aedc4f9e103ad0dac640df5
Ezequiel Emeraldo <zekeemerald@gmail.com>
2025-12-22 16:34:14 +0700

    Dashboard Responsive

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     id("org.jetbrains.kotlin.kapt") version "1.9.20"
-    id("com.android.library") version "8.13.1" apply false
+    id("com.android.library") version "8.13.2" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.8.2" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
